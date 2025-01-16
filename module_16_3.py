6e7ee8e2ed4e612bb83e9766d7fdf9822a9109b0
Elena16-student <usovk@mail.ru>
2025-01-16 12:02:30 +0300

    Add files via upload

--- module_16_3.py
+++ module_16_3.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI, Path
 from typing import Annotated
 
-# import warnings
-# warnings.simplefilter("ignore", DeprecationWarning)
+import warnings
+warnings.simplefilter("ignore", DeprecationWarning)
 
 app = FastAPI()
 
@@ -12,7 +12,7 @@ users = {"1": "Имя: Example, возраст: 18"}
 async def welcome():
     return 'Здравствуйте!Вы на главной странице'
 
-@app.get("/user")
+@app.get("/users")
 async def get_users() -> dict:
     return users
 
@@ -20,21 +20,21 @@ async def get_users() -> dict:
 @app.post("/user/{username}/{age}")
 async def create_user(username: Annotated[str, Path(min_length=5, max_length=20,  description="Enter username", example="UrbanUser", )]
                       , age: Annotated[int, Path(ge=18, le=90, description="Enter age", example="26")]) -> str:
-    current_user = str(int(max(users, key=int)) + 1)
-    users[current_user] = f"Имя: {username}, возраст: {age}"
-    return f"User {current_user} is registered"
+    new_user = str(int(max(users, key=int)) + 1)
+    users[new_user] = f"Имя: {username}, возраст: {age}"
+    return f"User {new_user} is registered"
 
 
 @app.put("/user/{user_id}/{username}/{age}")
-async def update_user(user_id: Annotated[int, Path(gt=0, description="Enter user_id")], username: Annotated[
+async def update_user(user_id: Annotated[str, Path(description="Enter user_id")], username: Annotated[
     str, Path(min_length=5, max_length=25, description="Enter username", example="UrbanProfy")]
                       , age: Annotated[int, Path(ge=18, le=90, description="Enter age", example="28")]) -> str:
     users[user_id] = f"Имя: {username}, возраст: {age}"
-    return f"The user {user_id} is updated"
+    return f"Пользователь {user_id} обновлен"
 
 
 @app.delete("/user/{user_id}")
-async def delete_user(user_id: Annotated[int, Path(gt=0, description="Enter user_id")]) -> str:
+async def delete_user(user_id: Annotated[str, Path(description="Enter user_id")]) -> str:
     users.pop(str(user_id))
-    return f"User {user_id} has been deleted"
+    return f"Пользователь {user_id} удален"
 
